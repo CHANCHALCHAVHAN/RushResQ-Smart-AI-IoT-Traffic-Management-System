c21086b79943128d11644aff4183889cc4bc56e4
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-10-10 19:18:48 +0530

    Update real_time_traffic_analysis.py

--- real_time_traffic_analysis.py
+++ real_time_traffic_analysis.py
@@ -9,3 +9,25 @@ best_model = YOLO('models/best.pt')
 # Define the threshold for considering traffic as heavy
 heavy_traffic_threshold = 10
 
+# Define the vertices for the quadrilaterals
+vertices1 = np.array([(465, 350), (609, 350), (510, 630), (2, 630)], dtype=np.int32)
+vertices2 = np.array([(678, 350), (815, 350), (1203, 630), (743, 630)], dtype=np.int32)
+
+# Define the vertical range for the slice and lane threshold
+x1, x2 = 325, 635 
+lane_threshold = 609
+
+# Define the positions for the text annotations on the image
+text_position_left_lane = (10, 50)
+text_position_right_lane = (820, 50)
+intensity_position_left_lane = (10, 100)
+intensity_position_right_lane = (820, 100)
+
+# Define font, scale, and colors for the annotations
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 1
+font_color = (255, 255, 255)    # White color for text
+background_color = (0, 0, 255)  # Red background for text
+
+
+        
