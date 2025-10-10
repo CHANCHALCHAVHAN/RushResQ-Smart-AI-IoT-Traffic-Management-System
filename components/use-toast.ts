52b72e71d70f7685e2a450c886f69e3a6d7c1e01
CHANCHALCHAVHAN <sakuchavhan08@gmail.com>
2025-10-10 22:50:58 +0530

    packages to install

---
+++ components/use-toast.ts
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+export const ToastContext = React.createContext();
+
+export const useToast = () => {
+  const context = React.useContext(ToastContext);
+  if (!context) {
+    throw new Error("useToast must be used within a ToastProvider");
+  }
+  return context;
+};
+
+export const ToastProvider = ({ children }) => {
+  const [toasts, setToasts] = React.useState([]);
+
+  const addToast = (toast) => {
+    setToasts((prev) => [...prev, toast]);
+  };
+
+  const removeToast = (id) => {
+    setToasts((prev) => prev.filter((toast) => toast.id !== id));
+  };
+
+  return (
+    <ToastContext.Provider value={{ toasts, addToast, removeToast }}>
+      {children}
+    </ToastContext.Provider>
+  );
+};
